38f57af198bc719a04d19c919ffed7393df7d0bc
Igor-A-Rodrigues <arantes992@gmail.com>
2025-02-20 11:25:59 +0000

    Update app.js

--- app.js
+++ app.js
@@ -6,15 +6,6 @@ let attempts = 1
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
-    if ('speechSynthesis' in window) {
-        let utterance = new SpeechSynthesisUtterance(texto);
-        utterance.lang = 'en-UK'; 
-        utterance.rate = 1.2; 
-        window.speechSynthesis.speak(utterance); 
-    } else {
-        console.log("Web Speech API não suportada neste navegador.");
-    }
-}
 
 function firstGreetings(){
     exibirTextoNaTela('h1', 'The secret number game');
